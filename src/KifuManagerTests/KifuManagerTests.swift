70d73c0b083d1d3c67d988967dae5de0cbf031ee
wryun <211150196+wryun-lab@users.noreply.github.com>
2025-05-25 02:02:40 +0900

    iOSアプリのテンプレート作成

---
+++ src/KifuManagerTests/KifuManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  KifuManagerTests.swift
+//  KifuManagerTests
+//
+//  Created by Takashi Takeuchi on 2025/05/25.
+//
+
+import Testing
+@testable import KifuManager
+
+struct KifuManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
